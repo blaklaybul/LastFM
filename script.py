9b6a9739409b6daa56b38e59e6f07b408f25089b
blaklaybul <michaelahirsch@gmail.com>
2015-02-16 16:46:30 +0100

    done?

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import requests as re
 import json
 
 # API Key: efd4d182fb0ea0f3fa61ca00dc96d052
@@ -11,23 +10,22 @@ artistUrl = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&page=1
 tagUrl = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&api_key=efd4d182fb0ea0f3fa61ca00dc96d052&format=json&mbid="
 
 i=0
-j=0
 
 df_artists = pd.read_json(artistUrl)
 
 #dataframes to be populated
 topArtists = pd.DataFrame(columns = ["name", "plays", "listeners"])
 topTags = pd.DataFrame(columns = ["mbid","tag","count"])
-tagPairs = pd.DataFrame(columns = ["tag1","tag2","pairs"])
 
 #have artists dataframe with their id, name, playcount, and number of listeners
 for artist in df_artists["artists"][1]:
 		topArtists.loc[artist["mbid"]] = [artist["name"], artist["playcount"], artist["listeners"]]
 
-#remove artists without an mbid
+#remove artists without an mbid.. there were a few.
 topArtists = topArtists[topArtists.index !='']
 
-#methods for populating tags dataframe.. in case of json request error, make sure artists are not repeated
+#methods for populating tags dataframe
+#in case of json request error, exclude makes sure artists are not repeated in subsequent requests.
 def populateTags():
 	exclude = pd.unique(topTags.mbid)
 	for artist in topArtists[~topArtists.index.isin(exclude)].index:
@@ -46,7 +44,6 @@ populateTags()
 #get rid of tags with count = 0, they don't appear on lastfm artist pages.
 topTags["count"] = topTags["count"].astype(int)
 topTags = topTags[topTags["count"] > 0]
-topTags["tag"] =  topTags['tag'].encode('utf8')
 
 #create an aggregate table to see how many instances of each tag we have across all artists
 #we will want to check what percent of a tags total occurrences appear with another tag.
@@ -75,6 +72,7 @@ tagGroups = tagGroups[tagGroups["min"]>=15]
 tagGroups.columns = ["tag_1", "tag_2", "count"]
 
 #only need to group by first column, since all tags are stored as (a,b) and (b,a)
+#this gives us how many times each tag appears in a pair
 aggPairs = tagGroups.groupby(tagGroups.tag_1, as_index=False).sum()
 aggPairs.columns = ["tag", "number_of_pairs"]
 
@@ -93,5 +91,13 @@ tagGroupsAnalyze.rename(columns = {"number_of_pairs":"number_of_pairs_y"}, inpla
 tagGroupsAnalyze["totalPairs"]=tagGroupsAnalyze.sum(axis=0)["count"]
 
 #We assign a strength of pair metric that is determined by the number of times the tags appear together divided by the sum of the total pairs they appear in individually.
-tagGroupsAnalyze["strength_of_pair"] = tagGroupsAnalyze.apply(lambda row: float(row["count"]) / (row["number_of_pairs_x"]+row["number_of_pairs_y"]), axis=1)
+tagGroupsAnalyze["pair_percent_of_total"] = tagGroupsAnalyze.apply(lambda row: float(row["count"]) / (row["totalPairs"]), axis=1)
+
+tagGroupsAnalyze["pair_percent_of_x"] = tagGroupsAnalyze.apply(lambda row: float(row["count"]) / (row["number_of_pairs_x"]), axis=1)
+
+tagGroupsAnalyze["pair_percent_of_y"] = tagGroupsAnalyze.apply(lambda row: float(row["count"]) / (row["number_of_pairs_y"]), axis=1)
+
+tagGroupsAnalyze["perc_x_y_sum"] = tagGroupsAnalyze.pair_percent_of_x + tagGroupsAnalyze.pair_percent_of_y
+
+tagGroupsAnalyze["score"] = (tagGroupsAnalyze.pair_percent_of_x*tagGroupsAnalyze.pair_percent_of_y)*tagGroupsAnalyze.pair_percent_of_total*1000000
 
